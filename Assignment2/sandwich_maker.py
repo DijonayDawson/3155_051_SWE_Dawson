d56ec61ea047c22e46451334f05097e81b23bcaa
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-02-16 12:52:49 -0500

    sandwichMaker methods
    I added sandwichMaker methods.

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -4,7 +4,17 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        if (self.machine_resources['bread'] >= ingredients['bread'] and
+                self.machine_resources['ham'] >= ingredients['ham'] and
+                self.machine_resources['cheese'] >= ingredients['cheese']):
+            return True
+        else:
+            return False
+
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        self.machine_resources['bread'] -= order_ingredients['bread']
+        self.machine_resources['ham'] -= order_ingredients['ham']
+        self.machine_resources['cheese'] -= order_ingredients['cheese']
+
+        print(sandwich_size + " sandwich is ready. Bon appetit!")
