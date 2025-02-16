a11e7ca4e0be071033aa68002247036b7081506c
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-02-16 12:53:44 -0500

    cashier methods
    I added cashier methods

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -5,9 +5,23 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+        largeDollars = float(input("how many large dollars?: "))
+        halfDollars = float(input("how many half dollars?: "))
+        quarters = float(input("how many quarters?: "))
+        nickels = float(input("how many nickels?: "))
+
+        total = largeDollars + halfDollars / 2 + quarters / 4 + nickels / 20
+
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = coins - cost
+            print("Here is $" + str(change) + " in change.")
+            return True
+        else:
+            print("Sorry, that's not enough money. Money refunded.")
+            return False
