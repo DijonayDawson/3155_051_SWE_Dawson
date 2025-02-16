c8859d8b2442a97a31f88e15ce9618cfb39bea99
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-02-16 12:53:21 -0500

    main method
    added main method

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,14 +6,31 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+SandwichMachine = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 
 
 
 def main():
     ###  write the rest of the codes ###
+    while 1==1:
+        intro = input("What would you like? (small/ medium/ large/ off/ report):")
+
+        if intro == "off":
+            exit()
+        elif intro == "report":
+            print("Bread: " + str(SandwichMachine.machine_resources["bread"]) + " slice(s)")
+            print("Ham: " + str(SandwichMachine.machine_resources["ham"]) + " slice(s)")
+            print("Cheese: " + str(SandwichMachine.machine_resources["cheese"]) + " pound(s)")
+
+        elif intro == "small" or "medium" or "large":
+            if SandwichMachine.check_resources(recipes[intro]['ingredients']) == True:
+                coins = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(coins, recipes[intro]['cost']) == True:
+                    SandwichMachine.make_sandwich(intro, recipes[intro]['ingredients'])
+            else:
+                print('Sorry, there is not enough bread')
 
 if __name__=="__main__":
     main()
