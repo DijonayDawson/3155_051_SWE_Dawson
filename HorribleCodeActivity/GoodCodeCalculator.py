e6a5b6677a2cee1614cb2a653eb008c78217281c
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-02-19 23:31:53 -0500

    corrected my good code
    clean my code to make sure it worked

--- HorribleCodeActivity/GoodCodeCalculator.py
+++ HorribleCodeActivity/GoodCodeCalculator.py
@@ -1,22 +1,27 @@
 class Calculator:
 
-    def addition(A,B):
+    def add(self, A, B):
         # This function will return the sum of A and B.
         return A + B
 
-    def subtraction(A,B):
+    def subtract(self, A, B):
         # This function will return the difference of A and B.
         return A - B
 
-    def multiplication(A,B):
+    def multiply(self, A, B):
         # This function will return the product of A and B.
         return A * B
 
-    def division(A,B):
+    def divide(self, A, B):
         # This function will return the quotient of A and B, except when y is a zero.
         if B == 0:
             return "error: cannot divide by 0"
-        return A / B:
+        return A / B
 
-    
-        
+# Instances to check my code
+calc = Calculator()
+print(calc.add(120, 92))       # This calculates 120 + 92
+print(calc.subtract(129, 45))    # This calculates 129 - 45
+print(calc.multiply(23, 12))  # This calculates 23 * 12
+print(calc.divide(8, 2))          # This calculates 8 / 2
+print(calc.divide(8, 0))          # This calculates 8 / 0
