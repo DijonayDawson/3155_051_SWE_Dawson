bb0412d8a5821b9088a77c2a82660fc67902ac7d
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-02-19 23:08:10 -0500

    creation of Goodcode
    created the good code calculator and add my basic functions

---
+++ HorribleCodeActivity/GoodCodeCalculator.py
@@ -0,0 +1,22 @@
+class Calculator:
+
+    def addition(A,B):
+        # This function will return the sum of A and B.
+        return A + B
+
+    def subtraction(A,B):
+        # This function will return the difference of A and B.
+        return A - B
+
+    def multiplication(A,B):
+        # This function will return the product of A and B.
+        return A * B
+
+    def division(A,B):
+        # This function will return the quotient of A and B, except when y is a zero.
+        if B == 0:
+            return "error: cannot divide by 0"
+        return A / B:
+
+    
+        
