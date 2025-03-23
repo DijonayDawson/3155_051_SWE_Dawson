ba61bd3e0a2d062b7028f720f5be91c4285e15c7
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-03-23 11:13:57 -0400

    updated db.py
    updated db.py

--- sheep_compendium/models/db.py
+++ sheep_compendium/models/db.py
@@ -1,18 +1,19 @@
 from models.models import Sheep
-from tuping import Dict
+from typing import Dict
+
 
 class FakeDB:
     def __init__(self):
-        self.Data: Dict[int, Sheep] = {}
+        self.data: Dict[int, Sheep] = {}
 
-    def get_sheep(self, id: int, sheep) -> Sheep:
+    def get_sheep(self, id: int) -> Sheep:
         return self.data.get(id)
 
 
 db = FakeDB()
 db.data = {
-    1: Sheep(id=1, name="Spice", breed="Gotland", sex="eve"),
-    2: Sheep(id=2, name="Blondie", breed="Polypax", sex="ram"),
+    1: Sheep(id=1, name="Spice", breed="Gotland", sex="ewe"),
+    2: Sheep(id=2, name="Blondie", breed="Polypay", sex="ram"),
     3: Sheep(id=3, name="Deedee", breed="Jacobs Four Horns", sex="ram"),
     4: Sheep(id=4, name="Rommy", breed="Romney", sex="ewe"),
     5: Sheep(id=5, name="Vala", breed="Valais Blacknose", sex="ewe"),
